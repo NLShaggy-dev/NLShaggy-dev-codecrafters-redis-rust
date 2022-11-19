e695f966626b1bc6f760947df3b190dbaadc34de
nlshaggy <viking2012df@gmail.com>
2022-11-19 12:49:12 -0700

    handle concurrent events

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
 // Uncomment this block to pass the first stage
 use std::io::{Read, Write};
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+
+fn handle_connection(mut stream: TcpStream) {
+    println!("accepted new connection");
+    let mut buf: [u8; 1024] = [0; 1024];
+    loop {
+        stream.read(&mut buf).unwrap();
+        stream.write("+PONG\r\n".as_bytes()).unwrap();
+    }
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -10,13 +20,10 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     for stream in listener.incoming() {
         match stream {
-           Ok(mut stream) => {
-                println!("accepted new connection");
-                let mut buf: [u8; 1024] = [0; 1024];
-                loop {
-                    stream.read(&mut buf).unwrap();
-                    stream.write("+PONG\r\n".as_bytes()).unwrap();
-                }
+           Ok(stream) => {
+                thread::spawn(move || {
+                    handle_connection(stream);
+                });
             }
             Err(e) => {
                println!("error: {}", e);
